9161f8def3afbadb672924baef221faa603241f5
linx01 <18810463426@163.com>
2018-12-05 10:53:01 +0800

    add 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+
+iDesktop Entry]
+Type=Application
+Name=Pycharm
+GenericName=Pycharm3
+Comment=Pycharm3:The Python IDE
+Exec=sh /snap/pycharm-community/99/bin/pycharm.sh
+Icon=/snap/pycharm-community/99/bin/pycharm.png
+Terminal=pycharm
+Categories=Pycharm;
